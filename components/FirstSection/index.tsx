e67ae17a680dea2572344167814e9db819616f58
artur9487 <artur.www9487@gmail.com>
2023-08-29 21:42:53 +0200

    Talwind changes

--- components/FirstSection/index.tsx
+++ components/FirstSection/index.tsx
@@ -16,78 +16,39 @@ const FirstSection: React.FC = () => {
 					margin: 'auto',
 					position: 'relative',
 				}}>
-				<Image
+				<img
+					src={'/green.jpg'}
+					alt='main photo'
+					className='object-cover'
+				/>
+				{/*<Image
 					src={'/green.jpg'}
 					objectFit='cover'
 					layout='fill'
 					alt='main photo'
-				/>
-				<Box
-					sx={{
-						bgcolor: 'rgba(255,255,255,0.4)',
-						width: '50%',
-						height: '100%',
-						overflow: 'hidden',
-						position: 'relative',
-					}}>
-					<Box
-						sx={{
-							position: 'absolute',
-							top: '40%',
-							right: 0,
-							transform: 'translateY(-50%)',
-						}}>
-						<Typography
-							sx={{
-								fontSize: 130,
-								color: 'white',
-								zIndex: 999,
-								letterSpacing: 10,
-								fontWeight: 1000,
-								fontFamily: 'Alatsi',
-								textShadow: '2px 2px rgb(215, 230, 239)',
-							}}>
+			/>*/}
+				<div className='bg-white/[.5] w-1/2 h-full overflow-hidden relative flex'>
+					<div className='absolute top-1/3 right-0'>
+						<span className='drop-shadow-lg text-9xl text-white z-999 font-alatsi font-bold tracking-normal'>
 							GREEN
-						</Typography>
-					</Box>
-				</Box>
-				<Stack
-					direction='column'
-					alignItems='center'
-					sx={{
-						width: '50%',
-						height: '100%',
-						bgcolor: 'rgba(255,255,255,0.85)',
-						mb: 40,
-						position: 'relative',
-					}}>
-					<div className='flex-3'></div>
-					<div className='border-4 border-indigo-600 p-10 flex-1'>
-						<span className='font-normal leading-8 text-black italic tracking-normal font-alatsi'>
-							Curabitur in risus sapien. Donec ex dolor, suscipit ut
-							nisi a, tincidunt laoreet nulla.
 						</span>
 					</div>
-					{/*	<Box sx={{ flex: 1, p: 10, border: 1 }}>
-						<Typography
-							sx={{
-								fontWeight: 500,
-								lineHeight: 2.5,
-								color: 'rgb(107, 107, 107)',
-								fontStyle: 'italic',
-								letterSpacing: 1.5,
-								fontFamily: 'Alatsi',
-							}}>
+				</div>
+				<div className='  relative flex flex-col w-1/2 h-full bg-white/[.5] overflow-hidden z-999'>
+					<div className='h-2/3 '></div>
+					<div className=' p-10 h-1/3 w-full '>
+						<span className=' font-normal leading-8 text-black italic tracking-normal font-alatsi'>
 							Curabitur in risus sapien. Donec ex dolor, suscipit ut
-							nisi a, tincidunt laoreet nulla.
-						</Typography>
-						</Box>*/}
-					<div className='absolute left-0 top-1/3'>
-						<div className='text-md text-9xl text-black z-999 font-alatsi font-bold tracking-normal Alatsi'>
+							nisi a, tincidunt laoreet nulla. Donec ex dolor,
+							suscipit ut nisi a, tincidunt laoreet nulla.
+						</span>
+					</div>
+					<div className='absolute left-0 top-1/3  h-2/3 w-full'>
+						<div className=' text-9xl text-black z-999 font-alatsi font-bold tracking-normal '>
 							LAND
 						</div>
 					</div>
-				</Stack>
+				</div>
 				<Stack
 					justifyContent='center'
 					alignItems='center'
