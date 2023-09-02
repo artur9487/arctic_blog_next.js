8717668f84e1c87980a86fef0456ea41f040cf4e
artur9487 <artur.www9487@gmail.com>
2023-09-03 00:00:59 +0200

    Adding Tailwind

--- components/FirstSection/index.tsx
+++ components/FirstSection/index.tsx
@@ -7,27 +7,15 @@ import AirIcon from '@mui/icons-material/Air';
 
 const FirstSection: React.FC = () => {
 	return (
-		<section>
-			<Stack
-				direction='row'
-				sx={{
-					width: '100%',
-					height: 600,
-					margin: 'auto',
-					position: 'relative',
-				}}>
-				<img
-					src={'/green.jpg'}
-					alt='main photo'
-					className='object-cover'
-				/>
-				{/*<Image
+		<section className=' relative'>
+			<div className='flex flex-row w-full h-96 m-auto relative '>
+				<Image
 					src={'/green.jpg'}
 					objectFit='cover'
 					layout='fill'
 					alt='main photo'
-			/>*/}
-				<div className='bg-white/[.5] w-1/2 h-full overflow-hidden relative flex'>
+				/>
+				<div className=' bg-white/[.5] w-1/2 h-full overflow-hidden relative flex'>
 					<div className='absolute top-1/3 right-0'>
 						<span className='drop-shadow-lg text-9xl text-white z-999 font-alatsi font-bold tracking-normal'>
 							GREEN
@@ -36,10 +24,10 @@ const FirstSection: React.FC = () => {
 				</div>
 				<div className='  relative flex flex-col w-1/2 h-full bg-white/[.5] overflow-hidden z-999'>
 					<div className='h-2/3 '></div>
-					<div className=' p-10 h-1/3 w-full '>
+					<div className=' p-4 h-1/3 w-full '>
 						<span className=' font-normal leading-8 text-black italic tracking-normal font-alatsi'>
 							Curabitur in risus sapien. Donec ex dolor, suscipit ut
-							nisi a, tincidunt laoreet nulla. Donec ex dolor,
+							Wł nisi a, tincidunt laoreet nulla. Donec ex dolor,
 							suscipit ut nisi a, tincidunt laoreet nulla.
 						</span>
 					</div>
@@ -49,6 +37,7 @@ const FirstSection: React.FC = () => {
 						</div>
 					</div>
 				</div>
+
 				<Stack
 					justifyContent='center'
 					alignItems='center'
@@ -60,8 +49,8 @@ const FirstSection: React.FC = () => {
 						height: 300,
 						boxShadow: '-12px 16px 14px -4px rgba(215, 230, 239, 1)',
 						position: 'absolute',
-						bottom: -70,
-						left: -70,
+						bottom: -100,
+						left: -100,
 					}}>
 					<Stack
 						alignItems='center'
@@ -89,12 +78,12 @@ const FirstSection: React.FC = () => {
 							sx={{ fontSize: 50, color: 'rgb(137, 241, 245)' }}
 						/>
 						<Stack direction='column'>
-							<Typography sx={{ fontWeight: 600, fontSize: 15 }}>
+							<span className='font-gray-400 text-xl font-alatsi font-thin'>
 								Snowy
-							</Typography>
-							<Typography sx={{ color: 'gray', fontSize: 13 }}>
+							</span>
+							<span className='font-gray-400 font-alatsi font-thin'>
 								50%
-							</Typography>
+							</span>
 						</Stack>
 					</Stack>
 					<Stack
@@ -115,7 +104,7 @@ const FirstSection: React.FC = () => {
 						</Stack>
 					</Stack>
 				</Stack>
-			</Stack>
+			</div>
 		</section>
 	);
 };
