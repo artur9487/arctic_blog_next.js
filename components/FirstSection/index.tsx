3d09e3c0e0be4b14ab690caa51c9305f60c6034d
artur9487 <artur.www9487@gmail.com>
2023-09-24 20:30:16 +0200

    Adding responsive changes

--- components/FirstSection/index.tsx
+++ components/FirstSection/index.tsx
@@ -6,9 +6,9 @@ import AirIcon from '@mui/icons-material/Air';
 import { useMediaQuery } from '@mui/material';
 
 const FirstSection: React.FC = () => {
-	const mediaQuery800 = useMediaQuery('min-width:800px');
+	const mediaQuery800 = useMediaQuery('min-width:1024px');
 	return (
-		<section className=' relative md:px-28 px-0'>
+		<section className=' relative lg:px-28 px-0'>
 			<div className='flex flex-row w-full h-96 relative '>
 				<Image
 					src={'/green.jpg'}
@@ -18,27 +18,27 @@ const FirstSection: React.FC = () => {
 				/>
 				<div className=' bg-white/[.5] w-1/2 h-full overflow-hidden relative flex'>
 					<div className='absolute top-1/3 right-0'>
-						<span className='drop-shadow-lg md:text-6xl lg:text-8xl xl:text-9xl text-white z-999 font-bold tracking-normal font-alatsi'>
+						<span className='drop-shadow-lg text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-white z-999 font-bold tracking-normal font-alatsi'>
 							GREEN
 						</span>
 					</div>
 				</div>
 				<div className=' relative flex flex-col w-1/2 h-full bg-white/[.5] overflow-hidden z-999'>
-					<div className='md:h-2/3 h-3/5'></div>
-					<div className=' md:p-4 h-1/3 w-full p-2'>
-						<span className='md:leading-8 leading-6 text-black italic font-semibold tracking-normal md:text-base text-xs'>
+					<div className='h-1/2 lg:h-3/5 lg:mt-4'></div>
+					<div className=' md:p-4 h-1/3 w-full p-2 '>
+						<span className='md:leading-4 leading-6 lg:text-base text-black italic font-semibold tracking-normal md:text-sm text-xs'>
 							Curabitur in risus sapien. Donec ex dolor, suscipit ut
 							Wł nisi a, tincidunt laoreet nulla. Donec ex dolor,
 							suscipit ut nisi a, tincidunt laoreet nulla.
 						</span>
 					</div>
 					<div className='absolute left-0 top-1/3  h-2/3 w-full'>
-						<div className='font-alatsi text-6xl md:text-9xl  text-black z-999 font-bold tracking-normal '>
+						<div className='drop-shadow-lg text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-black z-999 font-bold tracking-normal font-alatsi'>
 							LAND
 						</div>
 					</div>
 				</div>
-				<div className='md:py-12 py-6 md:px-8 px-4 gap-3 drop-shadow-lg rounded-xl justify-start items-start flex flex-col bg-white absolute left-2 md:-left-28 -bottom-14 md:-bottom-32'>
+				<div className='py-6 lg:py-10 lg:px-8 px-4 gap-3 drop-shadow-lg rounded-xl justify-start items-start flex flex-col bg-white absolute left-2 lg:-left-28 -bottom-14 lg:-bottom-32'>
 					<div className='flex gap-4 flex-row items-center '>
 						<CloudQueueIcon
 							sx={{
@@ -47,10 +47,10 @@ const FirstSection: React.FC = () => {
 							}}
 						/>
 						<div className='flex flex-col '>
-							<span className='text-black-600 font-bold md:text-xl text-sm'>
+							<span className='text-black-600 font-bold lg:text-xl text-sm'>
 								22 / 10 / 2022
 							</span>
-							<span className='text-slate-600 text-base md:font-thin text-sm'>
+							<span className='text-slate-600 lg:text-base md:font-thin text-sm'>
 								15C
 							</span>
 						</div>
@@ -64,10 +64,10 @@ const FirstSection: React.FC = () => {
 							}}
 						/>
 						<div className='flex flex-col'>
-							<span className='text-black-600 font-bold md:text-xl text-sm'>
+							<span className='text-black-600 font-bold lg:text-xl text-sm'>
 								Snowy
 							</span>
-							<span className='text-slate-600 text-base md:font-thin text-sm'>
+							<span className='text-slate-600 lg:text-base md:font-thin text-sm'>
 								50%
 							</span>
 						</div>
@@ -80,10 +80,10 @@ const FirstSection: React.FC = () => {
 							}}
 						/>
 						<div className='flex flex-col'>
-							<span className='text-black-600 font-bold md:text-xl text-sm'>
+							<span className='text-black-600 font-bold lg:text-xl text-sm'>
 								MPH West
 							</span>
-							<span className='text-slate-600 text-base md:font-thin text-sm'>
+							<span className='text-slate-600 lg:text-base md:font-thin text-sm'>
 								15C
 							</span>
 						</div>
--- components/SecondSection/index.tsx
+++ components/SecondSection/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const SecondSection: React.FC = () => {
 	return (
-		<section className='mt-48'>
+		<section className='lg:mt-44'>
 			<div className=' flex md:flex-row flex-col-reverse md:gap-32 gap-16 '>
 				<div className='justify-around flex md:flex-col flex-col-reverse relative w-full md:w-2/3 gap-8 md:gap-4'>
 					<div className='md:h-96 h-[200px]  md:w-full w-2/3 relative md:self-auto self-end'>
@@ -18,7 +18,7 @@ const SecondSection: React.FC = () => {
 					</div>
 
 					<div className='self-start w-2/3  text-justify md:self-auto'>
-						<span className='self-start italic md:text-lg text-base items-center tracking-wide leading-7 text-gray-500'>
+						<span className='self-start italic md:text-lg text-sm items-center tracking-wide leading-7 text-gray-500'>
 							Corem ipsum dolor sit amet, consectetur adipiscing elit.
 							Ut tempus, diam vitae laoreet tempor, libero risus
 							posere lorem, a commodo enim lorem nec lacus.
@@ -37,12 +37,12 @@ const SecondSection: React.FC = () => {
 						<div className='absolute -z-10 -bottom-4 -right-4 w-full h-full bg-gradient-to-r from-slate-200/[.5] to-slate-200/[.9]'></div>
 					</div>
 
-					<div className='md:w-2/3 m-full mr-auto '>
-						<div className='md:w-full mr-auto text-justify w-2/3'>
+					<div className='w-2/3 sm:w-full mr-auto '>
+						<div className=' mr-auto text-justify'>
 							<div className='text-2xl font-extrabold mb-8'>
 								Explore new world
 							</div>
-							<span className='italic md:text-lg text-base items-center tracking-wide leading-7 text-gray-500'>
+							<span className='italic md:text-lg text-sm items-center tracking-wide leading-7 text-gray-500'>
 								Lorem ipsum dolor sit amet, consectetur adipiscing
 								elit. Ut tempus, diam vitae laoreet tempor, libero
 								risus posuere lorem, a commodo enim lorem nec lacus.
